0ddaa3a996b80ce05a3c01f3cd78d135252f4b41
sajjat004 <sajjat18cse004@gmail.com>
2023-06-25 08:45:30 +0600

    Nabvar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar.compnent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <h3>Hello sajjat</h3>
     </div>
   );
 }
